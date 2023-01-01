5c0dba8190bc51a7f731c189666feaea3ee17382
RyanHoganGMIT <101425132+RyanHoganGMIT@users.noreply.github.com>
2023-01-01 18:51:48 +0000

    Added comments for steps.

--- Mastermind/MainPage.xaml.cs
+++ Mastermind/MainPage.xaml.cs
@@ -1,5 +1,18 @@
 ﻿namespace Mastermind;
 
+/* Steps for myself.
+ * 
+ * Generate a 4 colour code. Colours can repeat. 
+ * 6 Different colours. (I'll pick Red, Yellow, Green, Blue, White and Black).
+ * Black pegs = Correct colour in correct position.
+ * White pegs = Correct colour in wrong position.
+ * Allow user to make a guess by tapping the circles until they have the colour they want.
+ * Confirm button at bottom to confirm their guess.
+ * Compare the guess to the hidden code and place the black or white pegs.
+ * If they get the correct code, display an alert that they have won the game and display the code at the bottom of the screen.
+ * Complete the save and restart button functionalities.
+ */
+
 public partial class MainPage : ContentPage
 {
 	public MainPage()
