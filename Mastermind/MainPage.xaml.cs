b80749df9bee40f09bc2f3b31570b8d916db04f5
RyanHoganGMIT <101425132+RyanHoganGMIT@users.noreply.github.com>
2023-01-01 19:30:19 +0000

    Added a hidden confirm guess button. Added a test hidden code.

--- Mastermind/MainPage.xaml.cs
+++ Mastermind/MainPage.xaml.cs
@@ -13,21 +13,65 @@
  * Complete the save and restart button functionalities.
  */
 
+// Majority of comments will be taken from the above set of steps.
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    Random _random;
+    int[] _code = new int[4];
+
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+        _random = new Random();
+        GenerateHiddenCode();
+    }
 
+    // Complete the save and restart button functionalities.
     private void BtnRestart_Clicked(object sender, EventArgs e)
     {
-
     }
 
     private void BtnSave_Clicked(object sender, EventArgs e)
     {
+    }
 
+    private void BtnConfirm_Clicked(object sender, EventArgs e)
+    {
+    }
+
+    // Generate a 4 colour code. Colours can repeat.
+    // 6 Different colours. (I'll pick Red, Yellow, Green, Blue, White and Black).
+    private void GenerateHiddenCode()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int randomNum = _random.Next(1, 6);
+            
+            if (i == 0)
+            {
+                _code[i] = randomNum;
+                LblTest.Text = _code[0].ToString();
+            }
+            else if(i == 1)
+            {
+                _code[i] = randomNum;
+                LblTest2.Text = _code[1].ToString();
+            }
+            else if(i == 2)
+            {
+                _code[i] = randomNum;
+                LblTest3.Text = _code[2].ToString();
+            }
+            else if(i == 3)
+            {
+                _code[i] = randomNum;
+                LblTest4.Text = _code[3].ToString();
+            }
+            
+            
+            
+            
+        }
     }
 }
 
